5a4014d99457931b0695316f8820f2f3311d559b
Paul Schneider <paul.schneider4@gmail.com>
2025-12-04 00:44:21 -0500

    replace request module with requests library

--- walkscore_adapter.py
+++ walkscore_adapter.py
@@ -1,6 +1,5 @@
 
-from pydantic import BaseModel
-from request import HTTPRequest
+import requests
 from pydantic import BaseModel, Field, ValidationError
 from urllib.parse import urlencode
 
@@ -36,6 +35,19 @@ class WalkScoreData(BaseModel):
         # Allow extra fields that aren't defined
         extra = 'allow'
 
+def fetch_data(url: str):
+    
+    # Make a GET request
+    response = requests.get(url)
+
+    # Check the status code
+    if response.status_code == 200:
+        # Access the response content (e.g., JSON)
+        data = response.json()
+        return data
+
+    else:
+        raise Exception('API request failed. Status code: ' + str(response.status_code))
 
 def parse_walkscore(data: dict) -> WalkScoreData:
         try:
@@ -61,10 +73,9 @@ def get_walkscore_data(
 
     query_params = urlencode(params)
 
-    with HTTPRequest() as request:
-        response = request.get( WALKSCORE_API_ENPOINT + '?' + query_params)
-        walkscore_response = parse_walkscore(response['data'])
-        return walkscore_response
+    response = fetch_data( WALKSCORE_API_ENPOINT + '?' + query_params)
+    walkscore_response = parse_walkscore(response)
+    return walkscore_response
     
 def get_walkscore(
     lat: float,
